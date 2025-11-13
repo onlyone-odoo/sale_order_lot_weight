13336a5e8639e22e60bf1cb9063f378308b15834
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-13 20:18:19 -0300

    UPD traduccion es_AR.po RC1

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     "maintainers": ["onlyone-odoo"],
     "website": "https://onlyone.odoo.com/",
     "category": "Sales",
-    "version": "17.0.1.2.0",
+    "version": "17.0.1.2.1",
     "depends": ["stock", "sale", "account"],
     "data": [
         "views/stock_lot_views.xml",
