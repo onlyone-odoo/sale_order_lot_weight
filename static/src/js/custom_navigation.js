741b05b282ccd64786473a96ea3490accdd87138
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-17 15:10:56 -0300

    js operaciones detalladas ahora si 2

--- static/src/js/custom_navigation.js
+++ static/src/js/custom_navigation.js
@@ -1,43 +1,46 @@
+/** @odoo-module **/
 
+odoo.define('sale_order_lot_weight.custom_navigation', function (require) {
+    "use strict";
 
-import { ListRenderer } from "@web/views/list/list_renderer";
+    var ListRenderer = require('web.ListRenderer');
 
-ListRenderer.include({
-    events: _.extend({}, ListRenderer.prototype.events, {
-        'keydown .o_data_row td input': '_onKeydownInput',
-    }),
+    ListRenderer.include({
+        events: _.extend({}, ListRenderer.prototype.events, {
+            'keydown .o_data_row td input': '_onKeydownInput',
+        }),
 
-    _onKeydownInput: function (ev) {
-        this._super.apply(this, arguments);
-        if (ev.key === 'Enter') {
-            ev.stopPropagation();
-            const $input = $(ev.target);
-            const $td = $input.closest('td');
-            const fieldName = $td.data('field-name') || $input.data('field-name');
+        _onKeydownInput: function (ev) {
+            this._super.apply(this, arguments);
+            if (ev.key === 'Enter') {
+                ev.stopPropagation();
+                var $input = $(ev.target);
+                var $td = $input.closest('td');
+                var fieldName = $td.data('field-name') || $input.data('field-name');
+                var $row = $input.closest('tr');
 
-            if (fieldName === 'lot_name') {
-                // ENTER in lot_name -> focus weight in same row
-                const $row = $input.closest('tr');
-                const $weightInput = $row.find('input[name="weight"]');
-                if ($weightInput.length) {
-                    $weightInput.focus();
-                    $weightInput.select();
-                }
-            } else if (fieldName === 'weight') {
-                // ENTER in weight -> focus lot_name of next row or add new
-                const $row = $input.closest('tr');
-                const $nextRow = $row.next('tr.o_data_row');
-                if ($nextRow.length) {
-                    const $nextLotInput = $nextRow.find('input[name="lot_name"]');
-                    if ($nextLotInput.length) {
-                        $nextLotInput.focus();
-                        $nextLotInput.select();
+                if (fieldName === 'lot_name') {
+                    // ENTER in lot_name -> focus weight in same row
+                    var $weightInput = $row.find('input[name="weight"]');
+                    if ($weightInput.length) {
+                        $weightInput.focus();
+                        $weightInput.select();
                     }
-                } else {
-                    // Add new line
-                    this.trigger_up('add_line');
+                } else if (fieldName === 'weight') {
+                    // ENTER in weight -> focus lot_name of next row or add new
+                    var $nextRow = $row.next('tr.o_data_row');
+                    if ($nextRow.length) {
+                        var $nextLotInput = $nextRow.find('input[name="lot_name"]');
+                        if ($nextLotInput.length) {
+                            $nextLotInput.focus();
+                            $nextLotInput.select();
+                        }
+                    } else {
+                        // Add new line
+                        this.trigger_up('add_line');
+                    }
                 }
             }
-        }
-    },
+        },
+    });
 });
