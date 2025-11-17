2f68b377633e434c4a3e24182e5f0998655c2845
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-17 18:42:37 -0300

    update RC2 no create en sale_order

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMoveLine(models.Model):
@@ -8,8 +8,8 @@ class StockMoveLine(models.Model):
     weight = fields.Float(
         string="Weight (KG)",
         digits="Product Unit of Measure",
+        related="lot_id.weight",  # Related para mostrar peso de lote existente
+        readonly=False,  # Editable siempre (vista controla con readonly)
+        store=True,  # Stored para búsquedas y compute
         help="Weight in KG for this lot (editable only for new lots).",
     )
-
-    # Related para mostrar peso de lote existente, editable para nuevos (vista controla readonly)
-    weight = fields.Float(related="lot_id.weight", readonly=False, store=True)
