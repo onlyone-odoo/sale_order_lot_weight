ab25955cc3c27840c7dab20833f9083faa433519
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-18 20:42:58 -0300

    UPD stock picking report

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     "maintainers": ["onlyone-odoo"],
     "website": "https://onlyone.odoo.com/",
     "category": "Sales",
-    "version": "17.0.4.10.5",
+    "version": "17.0.4.11.5",
     "depends": ["stock", "sale", "account", "purchase"],
     "data": [
         "views/stock_lot_views.xml",
